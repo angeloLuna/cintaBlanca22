bc04ae5a2bb6f5950c27b28f73e42bd2b1c9bf40
angeloLuna <angel.luna.najera@gmail.com>
2018-08-06 20:20:50 -0500

    Importar fuentes y tipografías

---
+++ javascript/main.js
@@ -0,0 +1,85 @@
+// let numero = 10;
+
+// let string = "hola";
+
+// let arreglo = [2, 3];
+
+// let objeto ={};
+
+
+// console.log(  typeof numero  )
+
+// ----------------------
+// let numero = prompt("ingresa un numero");
+
+// numero = Number(numero);
+
+// console.log( numero +100);
+
+
+// -------------------------------
+// let edad = prompt("ingresa tu edad");
+
+// edad = Number(edad);
+
+// if (edad > 17){
+//   console.log("Puedes acceder");
+// }else{
+//   console.log("no puedes acceder")
+// }
+
+
+
+// Ingresar un nombre.
+
+// evaluar si es mayor a 5 caracteres.
+
+// let nombre = prompt("ingresa tu nombre");
+
+// let ext = nombre.length
+
+
+// if ( ext > 5) {
+//   console.log("tu nombre es muy largo");
+// }else{
+//   console.log("tu nombre es corto");
+// }
+
+
+// ------------------------------
+
+// let numero = 10;
+
+// if (numero > 0 && numero < 10) {
+//   console.log("es un numero de 1 al 9");
+// }else if (numero >= 10  && numero <= 20){
+//   console.log("numero entre 10 y 20");
+// }
+
+
+// -----------------
+
+let edad = 20;
+
+if (edad > 15) {
+  console.log("b15")
+  if (edad >= 18) {
+    console.log("18+")
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
